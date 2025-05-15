b6a585aea906310b6420a4fc92164257ae6126a4
Anupama Sharoon <119839951+ANUPAMASHAROON@users.noreply.github.com>
2025-05-15 09:44:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Substrings with same first and last characters/substrings-with-same-first-and-last-characters.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int countSubstring(string &s) {
+        int cnt = 0;
+        int n = s.length();
+        
+        unordered_map<char, int> map;
+        for(auto ch : s){
+            cnt += map[ch];
+            map[ch]++;
+        }
+        return (n+cnt);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+
+        Solution ob;
+
+        cout << ob.countSubstring(str) << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
