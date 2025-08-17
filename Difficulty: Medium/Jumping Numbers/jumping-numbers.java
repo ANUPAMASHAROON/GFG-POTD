7fd439e0cf607ee12d46debc5f50936dca58c49e
Anupama Sharoon <119839951+ANUPAMASHAROON@users.noreply.github.com>
2025-08-17 18:13:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Jumping Numbers/jumping-numbers.java
@@ -0,0 +1,26 @@
+class Solution {
+    static long jumpingNums(long X) {
+        long tmp = X; // for storing 1st digit of number X;
+        while(tmp>10){
+            tmp/=10;
+        }
+        long ans = 0;
+        Queue<Long> q = new ArrayDeque<>();
+        for(int i = 1;i<=tmp;i++){
+            q.offer((long)i);
+        }
+        while(!q.isEmpty()){
+            long curr = q.poll();
+            if(curr>X) continue;
+            ans = Math.max(ans,curr);
+            long d = curr%10;
+            if(d!=9){
+                q.offer(curr*10+d+1);
+            }
+            if(d!=0){
+                q.offer(curr*10+d-1);
+            }
+        }
+        return ans;
+    }
+};
